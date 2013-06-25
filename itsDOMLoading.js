831f37564d203a9a6a5a86f9a5756f880d0850da
Rhys <rhys@dataminr.com>
2013-06-25 11:55:46 -0400

    fix bugs

--- itsDOMLoading.js
+++ itsDOMLoading.js
@@ -5,7 +5,9 @@ window.itsDOMLoading = (function() {
         triggers: {},
         observer: null,
         scope: "body",
-        selector: $ || document.querySelectorAll,
+        selector: $ || function(selector, el) {
+            return (el || document).querySelectorAll(selector)
+        },
 
         addTrigger: function(selector, callback){
             this.triggers[selector] = callback;
@@ -17,7 +19,7 @@ window.itsDOMLoading = (function() {
             this.observer = new MutationObserver(function(mutations){
                 mutations.forEach(function(mutation){
                     var added = [].slice.call(mutation.addedNodes, 0);
-                    added.forEach(function(index, node){
+                    added.forEach(function(node){
                         for(key in itsDOMLoading.triggers){
                             if(matches(node, key)){
                                 itsDOMLoading.triggers[key](node);
@@ -29,7 +31,7 @@ window.itsDOMLoading = (function() {
 
             (itsDOMLoading.scope.nodeType ?
                 [itsDOMLoading.scope] :
-                query(scope)
+                query(this.scope)
             ).forEach(function(element){
                 this.observer.observe(element, config);
             }.bind(this));
@@ -38,12 +40,12 @@ window.itsDOMLoading = (function() {
 
     var matches = function(element, selector){
         var parent = element.parentNode;
-        var els = itsDOMLoading.selector(selector);
+        var els = query(selector, parent);
         return els.indexOf(element) > -1;
     };
 
-    var query = function(selector){
-        return [].slice.call(itsDOMLoading.selector(selector), 0);
+    var query = function(selector, parent){
+        return [].slice.call(itsDOMLoading.selector(selector, parent), 0);
     };
 
     return itsDOMLoading;
